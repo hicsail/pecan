964af64694f4ca578bd0cd617189bd813b113b15
Mike Dietze <dietze@bu.edu>
2017-03-30 10:10:07 -0400

    SDGVM: * flushed out a lot of rows in the settings file * Added PFTs.txt to handle mapping from BETY pfts to default PFTs (would be better in the database) * updated default input.dat to have a single land use

--- models/sdgvm/R/write.config.SDGVM.R
+++ models/sdgvm/R/write.config.SDGVM.R
@@ -16,7 +16,7 @@
 ##' @name write.config.SDGVM
 ##' @title Write SDGVM configuration files
 ##' @param defaults list of defaults to process
-##' @param trait.samples vector of samples for a given trait
+##' @param trait.values vector of samples for a given trait
 ##' @param settings list of settings from pecan settings file
 ##' @param run.id id of run
 ##' @return configuration file for SDGVM for given run
@@ -80,30 +80,144 @@ write.config.SDGVM <- function(defaults, trait.values, settings, run.id) {
   input.text <- readLines(con = input.file, n = -1)
   
   ## (2) MET PATH
+  input.text[2] <- dirname(settings$run$inputs$met$path)
   
   ## (4) GLOBAL SOIL
+  input.text[4] <- dirname(settings$run$inputs$soil$path)
   
   ## (5) GLOBAL LAND USE
+  input.text[5] <- dirname(settings$run$inputs$lulc$path)
   
-  ## (6) CO2 PATH
-  
-  ## (7) CO2 SWITCH
-  
+  ## (6 & 7) CO2 PATH & CO2 SWITCH
+  if(is.null(settings$run$inputs$co2)){ ## no CO2 provided
+    input.text[7] <- as.character(abs(as.numeric(input.text[7]))) ## default conc
+  } else { ## CO2 file provided
+    input.text[6] <- settings$run$inputs$co2$path  ## includes filename
+    input.text[7] <- as.character(-abs(as.numeric(input.text[7]))) ## negative flag
+  }
+
   ## (8) DEFAULT DATA DIR
+  input.text[8] <- dirname(settings$run$inputs$defaults$path)
+  
+  ## (9) model version
+  
+  ## (10, 11) switches
+  
+  ## (13) RESTART READ DIR
+  input.text[13] <- outdir
+  
+  ## (14) OUTPUT WRITE DIR
+  input.text[14] <- outdir
+  
+  ## (16) RUN TYPE
+  ## Spin-up:		1 1 0
+  ## Run:       0 0 0
+  ## Spin + run: 1 0 0
+  input.text[16] <- "1 0 0"
+  
+  ## (17) START/END YEAR
+  input.text[17] <- paste(lubridate::year(as.Date(settings$run$start.date)),
+                          lubridate::year(as.Date(settings$run$end.date)))
+  
+  ## parse spaces to find blocks, fill in from bottom-up
+  breaks <- which(trimws(input.text) == "")
   
   ## LAND COVER TRANSITIONS
+  nb <- dplyr::last(breaks)
+  # for now, leaving the defaults
   
   ## SOIL
+  # for now, leaving the defaults
+  nb <- breaks[length(breaks)-1]
+  
+  ## LAT/LON
+  nb <- breaks[length(breaks)-2]
+  input.text[nb+1] <- paste(settings$run$site$lat,settings$run$site$lon)
   
   ## FIRE
+  # for now, leaving the defaults
+  nb <- breaks[length(breaks)-3]
   
   ## LAND COVER CLASSES
+  # needs to be more sophisticated, but start with equal area
+  nb <- breaks[length(breaks)-4]
+  npft <- length(trait.values) - 1
+  pft.names <- names(trait.values)
+  input.text[nb+1] <- paste("1",paste(rep(100/npft,npft),pft.names,collapse = " "))
   
   ## PFTS
-  #input.text <- append(input.text,PFT.string,after = 28)
+  parm.names <- c("name","mixing_parameter","photosynthetic_pathway","FOO","FOO","FOO",
+                  "AGEMX","wood_density","FOO","FOO","SLA","leaf_turnover_rate",
+                  "wood_turnover_rate","root_turnover_rate","FOO","FOO","FOO","FOO",
+                  "FOO","FOO","FOO","FOO","FOO","FOO","FOO","FOO",
+                  "FOO","clumping_factor","Vcmax","FOO","Jmax","FOO",
+                  "cuticular_cond","stomatal_slope")
+  nb <- breaks[length(breaks)-5:4] + c(1,-1)
+  default.pfts <- read.table(textConnection(input.text[nb[1]:nb[2]]),header=FALSE)
+  colnames(default.pfts) <- parm.names
+  updated.pfts <- default.pfts
+  pft.lut <- read.table(system.file("PFTs.txt",package = "PEcAn.SDGVM"),header=TRUE,stringsAsFactors = FALSE) ## PFT Look up table to map BETY pfts to defaults
+  for(i in 1:npft){
+    if(pft.names[i] %in% default.pfts$name){
+      ## Update PFTs that are among default list
+      j = which(default.pfts$name == pft.names[i])
+    } else { ## Add new PFTs
+      
+      ## which PFT is the 'template'
+      if(pft.names[i] %in% pft.lut$PFT){
+        j = which(pft.names[i] == pft.lut$PFT)
+        j = which(default.pfts$name == pft.lut$Default[j])
+      } else {
+        ## if match not found, set to a default
+        j = which(default.pfts$name == "Dc_Bl")
+      }
+      
+      ## clone defaults to a new row
+      updated.pfts <- rbind(updated.pfts,default.pfts[j,])
+      j = nrow(updated.pfts)
+      updated.pfts$name[j] = pft.names[i]
+      
+    }
+    
+    ## update individual parameter values
+    pft <- trait.values[[i]]
+    for(k in seq_along(pft)){
+      
+      ## photosynthetic_pathway
+      # translate 3/4/5 for C3/C4/CAM to 0=C4, 1=C3
+    
+      ## SLA m2/kg -> m2/g DW
+      updated.pfts$SLA[j] = udunits2::ud.convert(pft$SLA,"m2/kg","m2/g") 
+    
+      ## leaf_turnover_rate (yr-1) -> leaf life span (days)
+    
+      ## wood_turnover_rate (yr-1) -> stem life span (days)
+    
+      ## root_turnover_rate (yr-1) -> root life span (days)
+    
+      ## Convert Jmax to Jmax/Vcmax
+    
+      ## stomatal slope and intercept units? (Bety g0 = umol H2O m-2 s-1)
+    
+    } ## END loop over traits
+    
+  }
   
+  ## write updated PFTs
+  
+  ## write new PFTs
+  #input.text <- append(input.text,PFT.string,after = 28)
   
   ## write general input file
   writeLines(input.text, con = input.file)  
+  
+  ###########################################
+  ## write additional parameters in param.dat
+  param.file <- file.path(rundir,"param.dat")
+  param.text <- readLines(con = param.file, n = -1)
+  
+  writeLines(param.text, con = param.file)  
+  
+  ## write additional parameters in misc_params.dat
   
 } # write.config.SDGVM
