f7b2c26038f4fcca5399397eb77855c7e8d14c02
Christopher Tate <computate@computate.org>
2023-09-09 23:10:24 -0600

    Containerizing the hf_landscape scripts with environment variables

--- scripts/HARV_metdownload_efi.R
+++ scripts/HARV_metdownload_efi.R
@@ -1,6 +1,14 @@
 #!/usr/bin/env Rscript
 #load libraries
-.libPaths(c("/projectnb/dietzelab/dietze/test-pecan/R/library",.libPaths()))
+# pecan_home example: /pecan
+pecan_home <- Sys.getenv("PECAN_HOME")
+# nc_dir example: /projectnb/dietzelab/ahelgeso/NOAA_met_data/
+nc_dir <- Sys.getenv("NC_DIR")
+# clim_dir example: /projectnb/dietzelab/ahelgeso/NOAA_met_data_CH1/
+clim_dir <- Sys.getenv("CLIM_DIR")
+source(paste0(pecan_home, "/modules/data.atmosphere/R/download_noaa_gefs_efi.R"))
+source(paste0(pecan_home, "/modules/data.atmosphere/R/noaa_gefs_efi_helper.R"))
+
 library(dplyr)
 option_list = list(optparse::make_option("--start.date",
                                          default = Sys.Date(),
@@ -15,14 +23,12 @@ jumpback = args$jumpback
 
 #set fcn inputs
 startdate = as.Date(args$start.date)
-nc_dir = "/projectnb/dietzelab/ahelgeso/NOAA_met_data/"
 site.lat = 42.5
 site.lon = -72.15
 sitename <- "HARV"
 siteid <- 646
-clim_dir <- "/projectnb/dietzelab/ahelgeso/NOAA_met_data_CH1/"
-runDays <- format(seq(from=as.Date(startdate - lubridate::days(jumpback)),
-               to=as.Date(as.character(startdate)),
+runDays <- format(seq(from=as.POSIXlt(startdate) - lubridate::days(jumpback),
+               to=as.POSIXlt(as.character(startdate)),
                by="days"),"%Y-%m-%d")
 print("args set")
 
@@ -30,13 +36,13 @@ for(t in seq_along(runDays)){
   print(runDays[t])
   
   #download met using EFI fcns
-  tmp = PEcAn.data.atmosphere:::download.NOAA_GEFS_EFI(sitename = sitename,
+  tmp = download.NOAA_GEFS_EFI(sitename = sitename,
                                                  outfolder = nc_dir,
                                                  start_date = runDays[t],
                                                  site.lat = site.lat,
                                                  site.lon = site.lon)
   #set up path for met2model
-  output_path <- file.path("/projectnb/dietzelab/ahelgeso/NOAA_met_data/noaa/NOAAGEFS_1hr", sitename, runDays[t], "00")
+  output_path <- file.path(paste0(nc_dir, "/noaa/NOAAGEFS_1hr", sitename, runDays[t], "00"))
   ########## Met2Model For SIPNET ##############
   outfolder = file.path(clim_dir, "noaa_clim", sitename, runDays[t])
   if(!dir.exists(outfolder)){dir.create(outfolder, recursive = TRUE)}
@@ -58,6 +64,7 @@ for(t in seq_along(runDays)){
                                    year.fragment = TRUE) 
     
   } 
+  print(paste0("Look for data in: ", outfolder))
   
 }
 print("done")
